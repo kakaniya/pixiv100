7d3eaac88b6d58675db471e9ae25c58c4418b98b
kakaniya <myflasky@163.com>
2018-11-15 17:46:35 +0800

    get100pic

--- pixiv/spiders/pix.py
+++ pixiv/spiders/pix.py
@@ -21,17 +21,20 @@ class PixSpider(scrapy.Spider):
                 'accept': 'application/json',
                 'accept-encoding': 'gzip, deflate, br',
                 'accept-language': 'zh-CN, zh;q=0.9',
-                'upgrade-insecure-requests': 1,
+                'upgrade-insecure-requests': '1',
                 'content-type':'application/x-www-form-urlencoded',
+                'referer': 'https: // www.pixiv.net /',
                 'user-agent': 'Mozilla/5.0(WindowsNT6.1;Win64;x64) AppleWebKit/537.36(KHTML,Gecko) Chrome/70.0.3538.77 Safari/537.36',
     }
 
     list_url = 'member_illust.php?id={user_id}&type=illust'
-    user_id = 作者id
+    user_id = 1193008
     all = 'ajax/user/{user_id}/profile/all'
 
-    get_url = 'https://www.pixiv.net/ajax/user/{user_id}/illusts?'
+    get_url = 'https://www.pixiv.net/ajax/user/{user_id}/profile/illusts?'
     full_url = None
+    page_count = 0
+    pic_count = 0
 
     def start_requests(self):
         yield Request(self.login_url,meta = {'cookiejar':1},callback=self.login_start)
@@ -44,10 +47,10 @@ class PixSpider(scrapy.Spider):
                                          meta={'cookiejar':response.meta['cookiejar']},
                                          headers = self.headers,
                                          formdata = {
-                                             'pixiv_id':'user_id',
+                                             'pixiv_id':'xxxxxx',
                                              'captcha':'',
                                              'g_recaptcha_response':'',
-                                             'password':'pass',
+                                             'password':'xxxxxxxxx',
                                              'post_key':post_key,
                                              'source':'pc',
                                              'ref':'wwwtop_accounts_index',
@@ -66,15 +69,48 @@ class PixSpider(scrapy.Spider):
         results = json.loads(response.text)
         #print(results)
         full_url = self.get_url.format(user_id=self.user_id)
+
         for result in results['body']['illusts']:
-                full_url += 'ids%5B%5D=' + str(result) +'&'
+            full_url += 'ids%5B%5D=' + str(result) +'&'
+            self.page_count += 1
+            if self.page_count >= 100:
+                break
+
+
         complite_url = full_url + 'is_manga_top=0'
-        print(complite_url)
+        #print(complite_url)
         yield Request(complite_url,meta = {'cookiejar':1},callback=self.parse_page)
 
 
     def parse_page(self,response):
         results = json.loads(response.text)
-        print ('start')
-        print(results)
+        print('start')
+        #print(results)
+        result = results['body']['works']
+        il_keys = result.keys()
+        #print(result)
+        for key in il_keys:
+            if result[key]['pageCount'] == 1:
+                get_pic_url = 'https://www.pixiv.net/ajax/illust/' + result[key]['id']
 
+                yield Request(get_pic_url,meta = {'cookiejar':1},callback=self.pic_get)
+
+
+    def pic_get(self,response):
+        results = json.loads(response.text)
+        url = results['body']['urls']['original']
+        headers_pic = {
+            'accept': 'application/json',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN, zh;q=0.9',
+            'upgrade-insecure-requests': '1',
+            'content-type': 'application/x-www-form-urlencoded',
+            'referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + results['body']['illustId'],
+            'user-agent': 'Mozilla/5.0(WindowsNT6.1;Win64;x64) AppleWebKit/537.36(KHTML,Gecko) Chrome/70.0.3538.77 Safari/537.36',
+        }
+        doc = requests.get(url,headers = headers_pic)
+        with open('c:/x/'+str(self.pic_count)+'.jpg','wb') as f:
+            f.write(doc.content)
+            f.close()
+
+        self.pic_count += 1
