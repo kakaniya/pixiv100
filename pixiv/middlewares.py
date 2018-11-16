c4634d71384e59077e480d7a016d2a3a756390cc
kakaniya <myflasky@163.com>
2018-11-16 17:10:50 +0800

    get pics

--- pixiv/middlewares.py
+++ pixiv/middlewares.py
@@ -104,4 +104,4 @@ class PixivDownloaderMiddleware(object):
 
 class ProxyMiddleware(object):
     def process_request(self,request,spider):
-        request.meta['proxy'] = 'http://127.0.0.1:xxxx'
+        request.meta['proxy'] = 'https://127.0.0.1:xxxx'
--- pixiv/spiders/pix.py
+++ pixiv/spiders/pix.py
@@ -5,6 +5,7 @@ import json
 from pyquery import PyQuery as pq
 import requests
 import logging
+import os
 
 
 logging.captureWarnings(True)
@@ -28,7 +29,7 @@ class PixSpider(scrapy.Spider):
     }
 
     list_url = 'member_illust.php?id={user_id}&type=illust'
-    user_id = 1193008
+    user_id = xxxxxx
     all = 'ajax/user/{user_id}/profile/all'
 
     get_url = 'https://www.pixiv.net/ajax/user/{user_id}/profile/illusts?'
@@ -36,8 +37,12 @@ class PixSpider(scrapy.Spider):
     page_count = 0
     pic_count = 0
 
+    os.makedirs('c:/pixiv/'+ str(user_id))
+
+
     def start_requests(self):
         yield Request(self.login_url,meta = {'cookiejar':1},callback=self.login_start)
+
 
     def login_start(self, response):
         print('start spider')
@@ -50,7 +55,7 @@ class PixSpider(scrapy.Spider):
                                              'pixiv_id':'xxxxxx',
                                              'captcha':'',
                                              'g_recaptcha_response':'',
-                                             'password':'xxxxxxxxx',
+                                             'password':'xxxxxxxx',
                                              'post_key':post_key,
                                              'source':'pc',
                                              'ref':'wwwtop_accounts_index',
@@ -58,6 +63,7 @@ class PixSpider(scrapy.Spider):
                                          },
                                          callback = self.after_login,
                                          dont_filter = True)
+
 
     def after_login(self,response):
         lists_url = response.url + self.list_url.format(user_id = str(self.user_id))
@@ -75,8 +81,6 @@ class PixSpider(scrapy.Spider):
             self.page_count += 1
             if self.page_count >= 100:
                 break
-
-
         complite_url = full_url + 'is_manga_top=0'
         #print(complite_url)
         yield Request(complite_url,meta = {'cookiejar':1},callback=self.parse_page)
@@ -92,13 +96,17 @@ class PixSpider(scrapy.Spider):
         for key in il_keys:
             if result[key]['pageCount'] == 1:
                 get_pic_url = 'https://www.pixiv.net/ajax/illust/' + result[key]['id']
-
                 yield Request(get_pic_url,meta = {'cookiejar':1},callback=self.pic_get)
+            elif result[key]['pageCount'] > 1:
+                get_pics_url = 'https://www.pixiv.net/ajax/illust/' + result[key]['id']
+                yield Request(get_pics_url, meta={'cookiejar': 1}, callback=self.pics_get)
 
 
     def pic_get(self,response):
         results = json.loads(response.text)
         url = results['body']['urls']['original']
+        title = results['body']['title']
+        user_name = results['body']['userName']
         headers_pic = {
             'accept': 'application/json',
             'accept-encoding': 'gzip, deflate, br',
@@ -109,8 +117,34 @@ class PixSpider(scrapy.Spider):
             'user-agent': 'Mozilla/5.0(WindowsNT6.1;Win64;x64) AppleWebKit/537.36(KHTML,Gecko) Chrome/70.0.3538.77 Safari/537.36',
         }
         doc = requests.get(url,headers = headers_pic)
-        with open('c:/x/'+str(self.pic_count)+'.jpg','wb') as f:
+        with open('c:/pixiv/'+ str(self.user_id) + '/' + title +'.jpg','wb') as f:
             f.write(doc.content)
             f.close()
 
-        self.pic_count += 1
+        #self.pic_count += 1
+
+
+    def pics_get(self,response):
+        results = json.loads(response.text)
+        url = results['body']['urls']['original']
+        title = results['body']['title']
+        page_count = results['body']['pageCount']
+        user_name = results['body']['userName']
+        headers_pic = {
+            'accept': 'application/json',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN, zh;q=0.9',
+            'upgrade-insecure-requests': '1',
+            'content-type': 'application/x-www-form-urlencoded',
+            'referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + results['body']['illustId'],
+            'user-agent': 'Mozilla/5.0(WindowsNT6.1;Win64;x64) AppleWebKit/537.36(KHTML,Gecko) Chrome/70.0.3538.77 Safari/537.36',
+        }
+        for num in range(int(page_count)):
+            doc = requests.get(url, headers=headers_pic)
+            with open('c:/pixiv/' + str(self.user_id) +'/' + title + '_' + str(num) + '.jpg', 'wb') as f:
+                f.write(doc.content)
+                f.close()
+
+            #self.pic_count += 1
+            p = 'p' + str(num)
+            url = url.replace(p,'p'+str(num+1))
